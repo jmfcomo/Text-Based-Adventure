b155df58ab2731302b6ee8208939fff382af9534
jmfcomo <43357248+jmfcomo@users.noreply.github.com>
2018-09-17 17:56:10 -0500

    0_0_0 Description
    Added prose for 0_0_0

--- Text_Adventure.py
+++ Text_Adventure.py
@@ -2,4 +2,6 @@
 x = 0
 z = 0
 y = 0
+curloc = 0_0_0
 #Area Descriptions
+0_0_0des = "Path    It is a cool and sunny day in autumn.  The breeze is gentle, but fiere in the same way, the clouds scarce.  You find yourself in a small Village.  The village is surrounded by grassland, leading to a castle in ruins, far off in the distance. You are on a path in the center of the village.  The path extends to your West, and to your East.  To your north is a small, run-down tavern. To your south is a general store.
