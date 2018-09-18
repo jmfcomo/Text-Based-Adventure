0f7607f99cbc6f3f581573728fd656ceca2ee276
jmfcomo <43357248+jmfcomo@users.noreply.github.com>
2018-09-17 21:22:05 -0500

    1.0.1 Prose Update
    Added prose for spawn location, and four locations around it.  Move mechanics coming soon

--- Text_Adventure.py
+++ Text_Adventure.py
@@ -3,5 +3,9 @@ x = 0
 z = 0
 y = 0
 curloc = 0_0_0
-#Area Descriptions
-0_0_0des = "Path    It is a cool and sunny day in autumn.  The breeze is gentle, but fiere in the same way, the clouds scarce.  You find yourself in a small Village.  The village is surrounded by grassland, leading to a castle in ruins, far off in the distance. You are on a path in the center of the village.  The path extends to your West, and to your East.  To your north is a small, run-down tavern. To your south is a general store.
+#Area Descriptions  x,z,y format  n denotes negative
+0_0_0des = "Path    It is a cool and sunny day in autumn.  The breeze is gentle, but fiere in the same way, the clouds scarce.  You find yourself in a small Village.  The village is surrounded by grassland, leading to a castle in ruins, far off in the distance. You are on a path in the center of the village.  The path extends to your West, and to your East.  To your north is a small, run-down tavern. To your south is a general store. "
+0_0_1ndes = "General Store  You find your self in an old general store.  Oil lanterns are burning, and the smell of cedar rises from the creaky floorboards."
+1_0_0des = "Path Junction   This is the junction of two ancient dirt paths.  They led north/south and east/west.  The east path leads into the grassland, to the castle beyond.  The west path leads back to the town.  The north and south paths led along the edge of the grassland, through cleared farmland beyond"
+1n_0_0des = "Path    To your north is the wall of the tavern.  To your south is a post office.  To your east and west, the path continues "
+0_0_1des = "Tavern   As you enter, the conversation stops, and the few customers turn to stare at you.  After a few seconds, they resume conversation, albeit more quiet, and muffled.  Along the west edge of the room is a door.  To your south is the exit you came in.  To the North and East, there are no exits, just the brick walls of the tavern. 
