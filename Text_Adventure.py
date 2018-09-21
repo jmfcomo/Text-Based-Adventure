dbfe62933fe229c48a643a0daf36360e120aa49a
jmfcomo <43357248+jmfcomo@users.noreply.github.com>
2018-09-21 13:01:17 -0500

    1.0.3.2
    Fixed some more syntax issues.
    Syntax is almost corrected entirely

--- Text_Adventure.py
+++ Text_Adventure.py
@@ -1,7 +1,6 @@
-#Luke: Adding a line so I can submit a pull request, and make comments.
 # Area Descriptions  x_z_y format  n denotes negative
 # This are displayed when an area is entered by being printed to the console
-0_0_0des = """Path
+des0_0_0 = """Path
     It is a cool and sunny day in autumn.
     The breeze is gentle, but fiere in the same way, the clouds scarce.
     You find yourself in a small Village.
@@ -11,7 +10,7 @@
     The path extends to your West, and to your East.
     To your north is a small, run-down tavern.
     To your south is a general store. """
-0_0_1des = """Tavern
+des0_0_1 = """Tavern
     As you enter, the conversation stops,
     and the few customers turn to stare at you.
     After a few seconds, they resume conversation,
@@ -20,19 +19,19 @@
     To your south is the exit you came in.
     To the North and East, there are no exits,
     just the brick walls of the tavern. """
-1_0_0des = """Path Junction
+des1_0_0 = """Path Junction
     This is the junction of two ancient dirt paths.
     They led north/south and east/west.
     The east path leads into the grassland, to the castle beyond.
     The west path leads back to the town.
     The north and south paths led along the edge of the grassland,
     through cleared farmland beyond. """
-0_0_1ndes = """General Store
+des0_0_1n = """General Store
     You find yourself in an old general store.
     Oil lanterns are burning,
     and the smell of cedar rises from the creaky floorboards.
     To the North is the door you came in."""
-1n_0_0des = """Path
+des1_0_0 = """Path
     To your north is the wall of the tavern.
     To your south is a post office.
     To your east and west, the path continues """
@@ -51,6 +50,8 @@ location = [0, 0, 0]
 
 # function for changing current location
 # Reads user input to change coordiats according to Move Mechanics
+
+
 def move(direction):
     if direction == "N" or "n" or "north" or "North":
         location += [x + y for x, y in zip(location, northmove)]
@@ -72,14 +73,16 @@ def move(direction):
 
 # Location Description defining
 # This part is very unpolished, and need complete renovation
-# Infinite loop which asks for user input, 
+# Infinite loop which asks for user input,
 # changes coordinate to new location,
 # and prints description for that area
 # except is for areas without a description
-# Line 81 is completly made-up, and very incorrect
+# Line following try statment needs work
+
+
 def des(location):
     try:
-        des = (x + "_" + z + "_" + z for x, y, z in location)
+        des = ("des" + x + "_" + z + "_" + z for x, y, z in location)
     except:
         print("""This area is currently unavaliable.
         Sorry for the inconvinience.
@@ -87,13 +90,14 @@ def des(location):
 
 # Location testing
 
+
 def moving():
     move(raw_input("What direction would you like to move?"))
     print(des)
 
 # Moving gameplay
-#This is the infinite loop that runs the above defined moving function
+# This is the infinite loop that runs the above defined moving function
 
 win = False
 while(win is False):
-    moving()
+    return(moving)
